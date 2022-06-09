f91ee2ad0f349d97ddaeee74c30597f0bd028a7b
Nicholas Samaras <n.samaras4@outlook.com>
2022-06-09 11:32:54 -0400

    ticket.jsx update

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import {useEffect} from 'react'
 import {toast} from 'react-toastify'
 import {useSelector, useDispatch} from 'react-redux'
-import {useParams} from 'react-router-dom'
-import {getTicket, reset} from '../features/tickets/ticketSlice'
+import {useParams, useNavigate, Navigate} from 'react-router-dom'
+import {getTicket, reset, closeTicket} from '../features/tickets/ticketSlice'
 import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 
@@ -24,6 +24,13 @@ function Ticket() {
         // eslint-disable-next-line
     }, [isError, message, ticketId])
 
+    // Close Ticket
+    const onTicketClose = () => {
+        dispatch(closeTicket(ticketId))
+        toast.success('Ticket Closed')
+        Navigate('/tickets')
+    }
+
     if(isLoading) {
         return <Spinner />
     }
@@ -43,12 +50,17 @@ function Ticket() {
               </span>
           </h2>
           <h3>Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}</h3>
+          <h3>Subclass: {ticket.product}</h3>
           <hr />
           <div className="ticket-desc">
               <h3>Description of Issue</h3>
               <p>{ticket.description}</p>
           </div>
       </header>
+
+      {ticket.status !== 'closed' && (
+          <button onClick={onTicketClose} className='btn btn-block btn-danger'>Close Ticket</button>
+      )}
   </div>
 }
 
